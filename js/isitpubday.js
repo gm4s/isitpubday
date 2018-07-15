b0278199091e16237161f6fd07b4c690eef32ef9
Guillaume M <nodens.gm@gmail.com>
2018-07-15 08:49:42 +0100

    create var for url

--- js/isitpubday.js
+++ js/isitpubday.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $.getJSON("https://api.giphy.com/v1/gifs/search?api_key=YFJteLQcYADoIP0ZkTaa7l4IZvp9eROG&lang=en&q=yes&rating=g&limit=40", function (json) {
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=YFJteLQcYADoIP0ZkTaa7l4IZvp9eROG&lang=en&q=yes&rating=g&limit=40"
+    $.getJSON(url, function (json) {
         var item = json.data[Math.floor(Math.random()*json.data.length)]
         $("#maingif").attr("src", item.images.original.url);
     });
