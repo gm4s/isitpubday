7683d9977c9a69ab0b294047930807d7d05cfdf6
Guillaume Mas <nodens.gm@gmail.com>
2020-03-24 11:06:58 +0000

    Update pub day status

--- js/isitpubday.js
+++ js/isitpubday.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var url = "https://api.giphy.com/v1/gifs/search?api_key=YFJteLQcYADoIP0ZkTaa7l4IZvp9eROG&lang=en&q=yes&rating=g&limit=40"
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=YFJteLQcYADoIP0ZkTaa7l4IZvp9eROG&lang=en&q=no&rating=g&limit=40"
     $.getJSON(url, function (json) {
         var item = json.data[Math.floor(Math.random()*json.data.length)]
         $("#maingif").attr("src", item.images.original.url);
